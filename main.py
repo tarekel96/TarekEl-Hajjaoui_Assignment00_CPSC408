bfce07b7ddcec63cb2730c441cfcd402a95f288e
Tarek <elhaj102@mail.chapman.edu>
2021-09-05 17:28:31 -0700

    OOP progress

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
 from random import randrange
 
 class MyMatrix():
-        def __init__(self, rows, columns, matrix):
+        # CLASS CONSTRUCTOR
+        def __init__(self, rows, columns, column_titles, output_file):
                 super().__init__()
+        # CLASS FIELDS
                 self.rows = rows
                 self.columns = columns
-                self.matrix = [[randrange(0, 1001) for x in range(columns)] for y in range(rows)]
-matrix_columns = ['A', 'B', 'C', 'D', 'E', 'F']
+                self.column_titles = column_titles
+                # create 2D int list via Python list comprehension filled with random numbers btwn 0 and 1001 (exclusive)
+                self.matrix = [[randrange(0, 1001) for x in range(self.columns)] for y in range(self.rows)]
+                self.output_file = output_file
+        
+        # CLASS METHODS
 
-# create 2D int list with the first parameter being number of rows and second being number of columns
-def create_matrix(rows = 400, columns = 6):
-        matrix = [[randrange(0, 1001) for x in range(columns)] for y in range(rows)]
-        return matrix
-
-# prints the given matrix to the console terminal window
-def print_matrix(matrix):
-        for row in matrix:
-                curr_row = ''
+                # writes the appropriate matrix to the file
+        def write_matrix_in_file(self):
+                inFile = open(self.output_file, 'wt') # write mode & text values
+                # write title row
                 col_counter = 0
-                for col in row:
-                        curr_row += str(col)
-                        if col_counter != len(row) - 1:
-                                curr_row += ','
+                for col in self.column_titles:
+                        if col_counter == len(self.column_titles) - 1:
+                                inFile.write(col)
+                        else:
+                                inFile.write(col + ',')
                         col_counter += 1
-                print(curr_row)
-
-# writes the appropriate matrix to the file
-def write_matrix_in_file(file):
-        inFile = open(file, 'wt') # write mode & text values
-        # write title row
-        for col in matrix_columns:
-                inFile.write(col)
-        # create 2D int list 
-        matrix = create_matrix(400, 6) # create a matrix with 400 rows and 6 columns
-        # write elements of 2D list with commas and line spaces between rows
-        for row in matrix:
-                curr_row = ''
-                col_counter = 0
-                for col in row:
-                        curr_row += str(col)
-                        if col_counter != 5:
-                                curr_row += ','
-                        col_counter += 1
-                inFile.write(curr_row)
                 inFile.write('\n')
-        inFile.close()
+                # write elements of 2D list with commas and line spaces between rows
+                for row in self.matrix:
+                        curr_row = ''
+                        col_counter = 0
+                        for col in row:
+                                curr_row += str(col)
+                                if col_counter != len(self.column_titles) - 1:
+                                        curr_row += ','
+                                col_counter += 1
+                        inFile.write(curr_row)
+                        inFile.write('\n')
+                inFile.close()
+        # prints the given matrix to the console terminal window
+        def print_matrix(self):
+                for row in self.matrix:
+                        curr_row = ''
+                        col_counter = 0
+                        for col in row:
+                                curr_row += str(col)
+                                if col_counter != len(row) - 1:
+                                        curr_row += ','
+                                col_counter += 1
+                        print(curr_row)
 
-matrix = create_matrix(3,2)
-print(len(matrix[0]))
-print(matrix)
-print_matrix(matrix)
-write_matrix_in_file('data.csv')
+def main():
+        myMatrix = MyMatrix(400, 6, ['A', 'B', 'C', 'D', 'E', 'F'], 'data.csv')
+        myMatrix.write_matrix_in_file()
+
+if __name__ == "__main__":
+    main()
